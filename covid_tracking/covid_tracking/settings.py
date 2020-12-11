8f2ea0658a5360aa4ae8b7ee2d2ae3c960366920
Sara Peksin <peksin.sara@gmail.com>
2020-12-11 17:56:01 +0100

    connecting to docker

--- covid_tracking/covid_tracking/settings.py
+++ covid_tracking/covid_tracking/settings.py
@@ -81,17 +81,29 @@ WSGI_APPLICATION = 'covid_tracking.wsgi.application'
   #      'NAME': BASE_DIR / 'db.sqlite3',
    # }
 #}
-
+import os
+if os.name == 'nt':
+    import platform
+    OSGEO4W = r"C:\OSGeo4W"
+    if '64' in platform.architecture()[0]:
+        OSGEO4W += "64"
+    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
+   
+   
+    
 #postgis
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        # TODO: use environment variables for database credentials
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'NAME': 'postgres'
-
+        'HOST': 'db', # set in docker-compose.yml
+        'PORT': 5432 # default postgres port
     }
 }
 
--- covid_tracking/reports/models.py
+++ covid_tracking/reports/models.py
@@ -1,3 +1,10 @@
+from django.contrib.gis.db.models import PointField
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Report(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    first_symptomatic = models.DateField(default=datetime.now)
+    location = PointField(null=True, blank=True)

c79e94f55b3e7c76e6b31fffdf604b05fc35892d
Sara Peksin <peksin.sara@gmail.com>
2020-12-12 15:18:34 +0100

    base views setup

--- covid_tracking/covid_tracking/settings.py
+++ covid_tracking/covid_tracking/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'covid_tracking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'covid_tracking/templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- covid_tracking/reports/views.py
+++ covid_tracking/reports/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Report
+from django.views.generic import ListView
 
 # Create your views here.
+
+
+class ReportListView(ListView):
+    model = Report
+    template_name = "reports/map.html"

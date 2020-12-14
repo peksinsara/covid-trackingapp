612a9815a784d8f46c48e01e690330d08e266206
Sara Peksin <peksin.sara@gmail.com>
2020-12-14 22:42:36 +0100

    bug fixes

--- start_postgis.sh
+++ start_postgis.sh
@@ -2,6 +2,6 @@ docker run \
 --name "covid_tracking-postgis" \
 -p 25432:5432 \
 -d -t kartoza/postgis \
--e POSTGRES=postres \
--e POSTGRES_PASS=postres \
--e POSTGRES_DBNAME=postres
+-e POSTGRES=postgres \
+-e POSTGRES_PASS=postgres \
+-e POSTGRES_DBNAME=postgres

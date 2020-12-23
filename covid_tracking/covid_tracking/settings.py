90c70ed47b1f2e38707e655d34985d578c9cbb30
Sara Peksin <peksin.sara@gmail.com>
2020-12-23 13:48:08 +0100

    heroku deplo

--- covid_tracking/covid_tracking/settings.py
+++ covid_tracking/covid_tracking/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +142,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
